a2118da8dfc56abde65d8b6f6741cd2e570356e3
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-12-25 00:38:13 +0200

    closes #3; Implemented Dark mode theming and toggling, added next themes package, created Dark mode button, temporary CSS enhancements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url(https://png.pngtree.com/background/20211215/original/pngtree-rainy-weather-picture-image_1483256.jpg)] bg-cover"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-[url(https://png.pngtree.com/background/20211215/original/pngtree-rainy-weather-picture-image_1483256.jpg)] bg-cover p-24"></main>
   );
 }
